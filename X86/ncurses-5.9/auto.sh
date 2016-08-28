b155b32cd1c8811c2580930b08568470642da26d
Jumbo88888 <physical2004@gmail.com>
2016-08-28 13:00:09 +0800

    Add ncurses-5.9.tar.gz

---
+++ X86/ncurses-5.9/auto.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+CURDIR=$PWD
+PACKAGE=$CURDIR/PACKAGE
+TARGET=$CURDIR/TARGET
+OBJ=$CURDIR/OBJ
+
+if [ ! -d "$PACKAGE" ]; then
+	mkdir $PACKAGE
+fi
+
+if [ ! -d "$TARGET" ]; then
+	mkdir $TARGET
+fi
+
+if [ ! -d "$OBJ" ]; then
+	mkdir $OBJ
+fi
+
+echo "************ ncurses-5.9.tar.gz  ***********************"
+if [ ! -d $TARGET/ncurses-5.9 ]; then
+    cd $PACKAGE
+    tar xvf "ncurses-5.9.tar.gz" -C $TARGET
+
+    cd $TARGET/ncurses-5.9/
+    patch -Np1 -d $TARGET/ncurses-5.9/ < $CURDIR/PATCH/MKlib_gen.patch
+
+    ./configure \
+       --prefix=$OBJ/ \
+       --with-shared \
+
+    make
+    make install
+fi
